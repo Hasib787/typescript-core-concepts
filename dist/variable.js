989d6a8df957877c1db04984527fdb0ecb8112b3
Hasib787 <mr.hasib45@gmail.com>
2021-02-13 19:58:18 +0600

    added sum function

--- dist/variable.js
+++ dist/variable.js
@@ -1,4 +1,14 @@
 "use strict";
-let money = 50;
-money = 10;
-console.log(money);
+let age = 25;
+let club = "Suhatun";
+const isFamous = false;
+console.log(club);
+function add(num1, num2) {
+    return num1 + num2;
+}
+const adding = add(25, 21);
+function fullName(firstName, lastName) {
+    return firstName + " " + lastName;
+}
+const user = fullName('Hasibul', 'Hasan');
+console.log(fullName);
