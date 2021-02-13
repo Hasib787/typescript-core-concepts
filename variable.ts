b2d5c871e0713507cc9e6e0ff7bfe1df66ab2c07
Hasib787 <mr.hasib45@gmail.com>
2021-02-13 20:56:33 +0600

    added a number and string function

--- variable.ts
+++ variable.ts
@@ -1,2 +1,17 @@
-let money = 50;
-money = 10;
+let age: number = 25;
+let club: string = "Suhatun";
+const isFamous: boolean = false;
+console.log(club);
+
+function add(num1: number, num2: number):number {
+    return num1 + num2;
+}
+const adding:number=add(25, 21);
+
+
+function fullName(firstName: string, lastName: string): string {
+    return firstName + " " + lastName;
+}
+
+const user = fullName('Hasibul', 'Hasan');
+console.log(fullName);
