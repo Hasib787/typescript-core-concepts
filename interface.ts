4c6f98596f4de201242b433b90745b41821c6c6f
Hasib787 <mr.hasib45@gmail.com>
2021-02-18 22:36:33 +0600

    added some function

---
+++ interface.ts
@@ -0,0 +1,22 @@
+interface Player {
+    club: string,
+    name: string,
+    salary: number,
+    wife?: string,
+    isPlaying: boolean
+}
+
+const messi: Player = {
+    name: 'messi',
+    club: 'barcelona',
+    salary: 4500000,
+    wife: 'sara',
+    isPlaying: true
+}
+
+const Ronaldo: Player = {
+    name: 'Ronaldo',
+    club: 'Liverpool',
+    salary: 5100000,
+    isPlaying: true
+}
