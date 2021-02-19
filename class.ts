ccc355a8339a67a4d0bfb706af33e9fb1faa57ec
Hasib787 <mr.hasib45@gmail.com>
2021-02-19 17:58:42 +0600

    some changes

--- class.ts
+++ class.ts
@@ -2,17 +2,17 @@ class Person {
     Name: string;
     private _partner: string;
     readonly fatherName: string;
-    constructor(Name: string, fatherName: string) {
+    constructor(Name: string, partner:string, father: string) {
         this.Name = Name;
-        this._partner = Name;
-        this.fatherName = fatherName;
+        this._partner = partner;
+        this.fatherName = father;
     }
     getName(): string {
         return this.Name + '' + this._partner;
     }
 }
-const sam = new Person('Samuel', 'David');
-console.log('name', sam.Name,);
+const sam = new Person('Samuel', 'David', 'Sara');
+console.log('name', sam.Name, sam.fatherName);
 const samName: string = sam.getName();
 sam.Name = 'Hasib';
 
