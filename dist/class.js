f3ad389d6a29a9b4c6ffcb1a6cb221c3da1613e8
Hasib787 <mr.hasib45@gmail.com>
2021-02-19 17:57:45 +0600

    added some chnages

--- dist/class.js
+++ dist/class.js
@@ -1,15 +1,15 @@
 "use strict";
 class Person {
-    constructor(Name, fatherName) {
+    constructor(Name, partner, father) {
         this.Name = Name;
-        this._partner = Name;
-        this.fatherName = fatherName;
+        this._partner = partner;
+        this.fatherName = father;
     }
     getName() {
         return this.Name + '' + this._partner;
     }
 }
-const sam = new Person('Samuel', 'David');
-console.log('name', sam.Name);
+const sam = new Person('Samuel', 'Sara', 'David');
+console.log('name', sam.Name, 'PartnerName', sam._partner, 'FatherName', sam.fatherName);
 const samName = sam.getName();
 sam.Name = 'Hasib';
