e24e95c87bef7780fad874e56a1f53e76106bdcf
Md. Masud Rana <mr.hasib45@gmail.com>
2022-01-05 23:38:44 +0600

    leared object

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 var myName = "Masud Rana Hasib";
 console.log(myName);
+
+var number;
+number = 10;
+var isHungry;
+
+var studentId = 154542;
+studentId = "wen2-44111";
+console.log(studentId);
+
--- index.ts
+++ index.ts
@@ -4,4 +4,23 @@ let number: number;
 number = 10;
 let isHungry: boolean;
 
+type stringOrNumber = string | number;
+let studentId: stringOrNumber = 154542;
+studentId = "wen2-44111";
+console.log(studentId);
+
+//object
+
+type PersonType = { name: string; age: number, hobby: string }
+let person1: PersonType = {
+  name: "Masud Rana Hasib",
+  age: 22,
+  hobby: "Coding",
+};
+
+let person2: PersonType = {
+    name: "Hasib",
+    age: 28,
+    hobby: "Sleeping",
+}; 
 
